4c4e1af757507f883a820b0fcbee6d9033bbd7d6
Tjaart Blignaut <tjaart@getbucks.com>
2019-01-25 15:39:11 +0200

    add rabbitmq settings where they belong

---
+++ MyBucks.Core.MessageQueue/ConfigurationModel/RabbitMqSettings.cs
@@ -0,0 +1,9 @@
+namespace MyBucks.Core.MessageQueue.ConfigurationModel
+{
+    public class RabbitMqSettings
+    {
+        public string RabbitMqHostname { get; set; }
+        public string RabbitMqPassword { get; set; }
+        public string RabbitMqUsername { get; set; }
+    }
+}
